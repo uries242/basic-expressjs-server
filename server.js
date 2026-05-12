e7348c2480059215ac37ead288fa10baa19d1e68
uries242 <teykoa@gmail.com>
2026-05-12 00:22:38 -0400

    Fixed syntax bugs

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/contact.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
